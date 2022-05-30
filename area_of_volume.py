6e088cd5a2862520e44ae4797dc2deaad019f55c
JoaoMarcos17 <joaomarcoscalacia@hotmail.com>
2022-05-30 08:47:40 +0100

    coding is hard

---
+++ area_of_volume.py
@@ -0,0 +1,19 @@
+# cubic area calculator
+
+width_area = int(input('enter the width of the area: '))
+
+length_area = int(input('enter the length of the area: '))
+
+# we have the squared area now
+
+area = width_area * length_area
+
+print(area)
+
+height_area = int(input('enter the height of the area: '))
+
+# the get the cubic area we multiply the first input with the last input 
+
+are_of_volume = area * height_area
+
+print('the volume is', are_of_volume)
